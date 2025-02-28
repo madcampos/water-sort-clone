7ee2852508d5e53fa772571ac0b405e8636b776b
madcampos <me@madcampos.dev>
2025-02-28 02:59:26 -0500

    feat: add new files

---
+++ js/audio.js
@@ -0,0 +1,2 @@
+// TODO: add sound effects support
+// Ref: https://developer.mozilla.org/en-US/docs/Games/Techniques/Audio_for_Web_Games
---
+++ js/i18n.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} string
+ */
+export function getTranslatedString(string) {}
+
+function translateDocument() {
+	// TODO: find all elements and translate them
+}
+
+export function initTranslations() {
+	// TODO: load json files
+	// TODO: replace strings
+	// TODO: rerender flasks
+	// TODO: reload status
+}
