3280b518d0ec76d2f3440d01b5188ab7203a63ce
madcampos <me@madcampos.dev>
2025-06-23 23:40:19 -0400

    feat: add flask status implementation

---
+++ src/components/FlaskStatus/index.ts
@@ -0,0 +1,34 @@
+import { html, LitElement } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+
+type AvailableFlaskStatus = 'deselected' | 'failedToPour' | 'pour' | 'reset' | 'selected';
+
+@customElement('flask-status')
+export class FlaskStatus extends LitElement {
+	@state()
+	accessor #currentStatus = '';
+
+	updateStatus(status: AvailableFlaskStatus, sourceIndex?: number, destIndex?: number) {
+		switch (status) {
+			case 'selected':
+				this.#currentStatus = `Flask ${(sourceIndex ?? 0) + 1} selected.`;
+				break;
+			case 'deselected':
+				this.#currentStatus = `Flask ${(sourceIndex ?? 0) + 1} deselected.`;
+				break;
+			case 'pour':
+				this.#currentStatus = `Poured from Flask ${(sourceIndex ?? 0) + 1} to Flask ${(destIndex ?? 0) + 1}.`;
+				break;
+			case 'failedToPour':
+				this.#currentStatus = `Cannot pour from Flask ${(sourceIndex ?? 0) + 1} to Flask ${(destIndex ?? 0) + 1}, please select another flask.`;
+				break;
+			case 'reset':
+			default:
+				this.#currentStatus = 'Select a flask.';
+		}
+	}
+
+	override render() {
+		return html`<output id="game-status" aria-live="polite">${this.#currentStatus}</output>`;
+	}
+}
